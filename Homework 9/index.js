06377d3e5a699b42b01be0861e61465dc2cda1e3
777-Adrian-777 <81689615+777-Adrian-777@users.noreply.github.com>
2021-07-01 17:45:51 +0300

    Update index.js

--- Homework 9/index.js
+++ Homework 9/index.js
@@ -1,1 +1,24 @@
 //Task 1
+
+//Task1
+$("a[href^='https://']").attr('target', '_blank');
+
+//Task2
+$("h2.head")
+  .css("background-color", "green")
+  .find(".inner")
+  .css("font-size", "35px");
+
+//Task3
+$("h3")
+  .next("div")
+  .each(function () {
+    $(this).insertBefore($(this).prev("h3"));
+  });
+
+//Task4
+$(".checkboxClass").on("click", function () {
+  if ($(".checkboxClass:checked").length >= 3) {
+    $(".checkboxClass").attr("disabled", "disabled");
+  }
+});
