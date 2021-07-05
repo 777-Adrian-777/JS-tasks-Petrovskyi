2d93e156437df55ae8032d726b454a372646949e
777-Adrian-777 <81689615+777-Adrian-777@users.noreply.github.com>
2021-07-05 16:23:34 +0300

    Update index.js

--- Homework 7/index.js
+++ Homework 7/index.js
@@ -175,5 +175,38 @@
 
 //Task 7
 
+var countriesSelect = document.querySelector('#countries-select');
+  var citiesSelect = document.querySelector('#cities-select');
+  var data = {
+        Germany: ['Berlin', 'Keln', 'Munich'],  
+        USA: ['New-York', 'Los-Angeles', 'Washington'], 
+        Ukraine: ['Lviv', 'Kyiv', 'Odesa'], 
+}
+
+var countries = Object.keys(data);
+addOptions(countriesSelect, countries);
+
+var defaultCities = data[countries[0]];
+addOptions(citiesSelect, defaultCities);
+
+countriesSelect.addEventListener('change', function() {
+  var cities = data[this.value];
+  citiesSelect.lenght = 0;
+  
+  addOptions(citiesSelect, cities);
+});
+
+function addOptions(select, arr) {
+  for (var i = 0; i < arr.length; i++) {
+    select.add(new Option(arr[i]));
+  }
+}
+
+</script>
+
+
+
+
+
 
 
